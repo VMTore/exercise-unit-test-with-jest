8ad9cc610c6943a4dc6deaaf7cb9fb3fbe6a886c
VMTore <151078881+VMTore@users.noreply.github.com>
2024-05-13 19:31:47 +0000

    añado el typeoff para verificar el tipo de valor

--- app.test.js
+++ app.test.js
@@ -6,13 +6,15 @@ const { fromDollarToYen } = require('./app.js');
 const { fromYenToPound } = require('./app.js');
 
 
-test("One euro should be 1.07 dollars", function() {
+test("One euro should be 1.07 dollars and the value is a number", function() {
     // Uso la función como debe ser usada
     const dollars = fromEuroToDollar(3.5);
     // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
     const expected = 3.5 * 1.07;
+    const expectedNumber = fromEuroToDollar(); 
     // Hago mi comparación (la prueba)
     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+    expect(typeof expectedNumber).toBe("number");
 })
 
 test("One dollar should be 156.5 yens", function() {
@@ -20,7 +22,9 @@ test("One dollar should be 156.5 yens", function() {
     // Si 1 dollar son 156.5 yenes, entonces 3.5 dollares debe ser (3.5 * 156.5)
     const expected = 3.5 * 156.5;
     // Hago mi comparación (la prueba)
+    const expectedNumber = fromDollarToYen();
     expect(fromDollarToYen(3.5)).toBe(547.75); // 1 dolar son 156.5 yenes, entonces 3.5 dolares deberían ser = (3.5 * 156.5)
+    expect(typeof expectedNumber).toBe("number");
 })
 
 test("One yen should be 0.0051 pounds", function() {
@@ -28,5 +32,7 @@ test("One yen should be 0.0051 pounds", function() {
     // Si 1 yen son 0.0051 pounds, entonces 3.5 yenes debe ser (3.5 * 0.0051)
     const expected = 3.5 * 0.0051;
     // Hago mi comparación (la prueba)
+    const expectedNumber = fromYenToPound();
     expect(fromYenToPound(3.5)).toBe(0.01785); // 1 yen son 0.0051 pounds, entonces 3.5 yenes deberían ser = (3.5 * 0.0051)
+    expect(typeof expectedNumber).toBe("number");
 })
